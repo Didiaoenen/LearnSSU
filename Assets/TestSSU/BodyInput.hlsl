f5ab1fa42d88f802e499ecb440daeff8e6b9844a
Didiaoenen <1191430879@qq.com>
2023-03-08 21:02:52 +0800

    add commit

--- Assets/TestSSU/BodyInput.hlsl
+++ Assets/TestSSU/BodyInput.hlsl
@@ -1,82 +1,148 @@
-#ifndef Lit
-#define Lit
+#ifndef BodyInput
+#define BodyInput
 
-#ifndef SHADER_API_D3D11
-#define SHADER_API_D3D11
+#include "Input.hlsl"
+#include "Head.hlsl"
+
+#include "Packages/com.unity.render-pipelines.universal/Shaders/LitForwardPass.hlsl"
+
+#ifndef _IS_ANGELRING_OFF
+#define _IS_ANGELRING_OFF
 #endif
-
-#ifndef fixed
-#define fixed float
-#endif
-
-#ifndef fixed3
-#define fixed3 float3
-#endif
-
-#include "Packages/com.unity.render-pipelines.core/ShaderLibrary/Common.hlsl"
-#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
-
-float4 LitVertex(float3 position : POSITION) : SV_Position
-{
-    return float4(position, 1.0);
-}
-
-float4 LitFragment() : SV_Target
-{
-    return float4(1.0, 1.0, 1.0, 1.0);
-}
 
 #if (SHADER_LIBRARY_VERSION_MAJOR == 7 && SHADER_LIBRARY_VERSION_MINOR >= 3) || (SHADER_LIBRARY_VERSION_MAJOR >= 8)
 
-#   ifdef _ADDITIONAL_LIGHTS
-#       ifndef REQUIRES_WORLD_SPACE_POS_INTERPOLATOR
-#           define REQUIRES_WORLD_SPACE_POS_INTERPOLATOR
-#       endif
-#   endif
+#ifdef _ADDITIONAL_LIGHTS
+#ifndef REQUIRES_WORLD_SPACE_POS_INTERPOLATOR
+#define REQUIRES_WORLD_SPACE_POS_INTERPOLATOR
+#endif
+#endif
 
 #else
 
-#   ifdef _MAIN_LIGHT_SHADOWS
+#ifdef _MAIN_LIGHT_SHADOWS
 //#     if !defined(_MAIN_LIGHT_SHADOWS_CASCADE) 
-#           ifndef REQUIRES_VERTEX_SHADOW_COORD_INTERPOLATOR
-#               define REQUIRES_VERTEX_SHADOW_COORD_INTERPOLATOR
-#           endif
+#ifndef REQUIRES_VERTEX_SHADOW_COORD_INTERPOLATOR
+#define REQUIRES_VERTEX_SHADOW_COORD_INTERPOLATOR
+#endif
 //#     endif
-#   endif
+#endif
 
-#   ifdef _ADDITIONAL_LIGHTS
-#       ifndef REQUIRES_WORLD_SPACE_POS_INTERPOLATOR
-#           define REQUIRES_WORLD_SPACE_POS_INTERPOLATOR
-#       endif
-#   endif
+#ifdef _ADDITIONAL_LIGHTS
+#ifndef REQUIRES_WORLD_SPACE_POS_INTERPOLATOR
+#define REQUIRES_WORLD_SPACE_POS_INTERPOLATOR
+#endif
+#endif
 
 #endif
 
 #define UNITY_PROJ_COORD(a) a
-#define UNITY_SAMPLE_SCREEN_SHADOW(tex, uv) tex2Dproj( tex, UNITY_PROJ_COORD(uv) ).r
+#define UNITY_SAMPLE_SCREEN_SHADOW(tex, uv) tex2Dproj(tex, UNITY_PROJ_COORD(uv)).r
 #define TEXTURE2D_SAMPLER2D(textureName, samplerName) Texture2D textureName; SamplerState samplerName
 
 TEXTURE2D_SAMPLER2D(_RaytracedHardShadow, sampler_RaytracedHardShadow);
 
 float4 _RaytracedHardShadow_TexelSize;
 
-//function to rotate the UV: RotateUV()
-//float2 rotatedUV = RotateUV(i.uv0, (_angular_Verocity*3.141592654), float2(0.5, 0.5), _Time.g);
-float2 RotateUV(float2 _uv, float _radian, float2 _piv, float _time)
+struct VertexInput
 {
-    float RotateUV_ang = _radian;
-    float RotateUV_cos = cos(_time * RotateUV_ang);
-    float RotateUV_sin = sin(_time * RotateUV_ang);
-    return (mul(_uv - _piv, float2x2(RotateUV_cos, -RotateUV_sin, RotateUV_sin, RotateUV_cos)) + _piv);
-}
+    float4 vertex : POSITION;
+    float3 normal : NORMAL;
+    float4 tangent : TANGENT;
+    float2 texcoord0 : TEXCOORD0;
+
+#if defined(_IS_ANGELRING_OFF)
+	float2 lightmapUV : TEXCOORD1;
+#elif defined(_IS_ANGELRING_ON)
+    float2 texcoord1 : TEXCOORD1;
+	float2 lightmapUV : TEXCOORD2;
+#endif
+    
+	UNITY_VERTEX_INPUT_INSTANCE_ID
+};
+
+struct VertexOutput
+{
+    float4 pos : SV_POSITION;
+    float2 uv0 : TEXCOORD0;
+
+//v.2.0.4
+#if defined(_IS_ANGELRING_OFF)
+    float4 posWorld : TEXCOORD1;
+    float3 normalDir : TEXCOORD2;
+    float3 tangentDir : TEXCOORD3;
+    float3 bitangentDir : TEXCOORD4;
+    //v.2.0.7
+    float mirrorFlag : TEXCOORD5;
+
+	DECLARE_LIGHTMAP_OR_SH(lightmapUV, vertexSH, 6);
+#if defined(_ADDITIONAL_LIGHTS_VERTEX) || (VERSION_LOWER(12, 0))
+    half4 fogFactorAndVertexLight : TEXCOORD7; // x: fogFactor, yzw: vertex light
+#else
+	    half fogFactor : TEXCOORD7; 
+#endif
+
+#ifndef _MAIN_LIGHT_SHADOWS
+    float4 positionCS : TEXCOORD8;
+    int mainLightID : TEXCOORD9;
+#else
+	float4 shadowCoord : TEXCOORD8;
+	float4 positionCS : TEXCOORD9;
+    int mainLightID : TEXCOORD10;
+#endif
+    
+	UNITY_VERTEX_INPUT_INSTANCE_ID
+	UNITY_VERTEX_OUTPUT_STEREO
 
 //
-fixed3 DecodeLightProbe(fixed3 N)
-{
-    return ShadeSH9(float4(N, 1));
-}
+#elif defined(_IS_ANGELRING_ON)
+    float2 uv1 : TEXCOORD1;
+    float4 posWorld : TEXCOORD2;
+    float3 normalDir : TEXCOORD3;
+    float3 tangentDir : TEXCOORD4;
+    float3 bitangentDir : TEXCOORD5;
+    //v.2.0.7
+    float mirrorFlag : TEXCOORD6;
 
-inline void InitializeStandardLitSurfaceDataUTS(float2 uv, out SurfaceData outSurfaceData)
+    DECLARE_LIGHTMAP_OR_SH(lightmapUV, vertexSH, 7);
+#if defined(_ADDITIONAL_LIGHTS_VERTEX) || (VERSION_LOWER(12, 0))
+    half4 fogFactorAndVertexLight   : TEXCOORD8; // x: fogFactor, yzw: vertex light
+#else
+	half  fogFactor					: TEXCOORD8; // x: fogFactor, yzw: vertex light
+#endif
+    
+#ifndef _MAIN_LIGHT_SHADOWS
+    float4 positionCS               : TEXCOORD9;
+    int   mainLightID              : TEXCOORD10;
+#else
+    float4 shadowCoord              : TEXCOORD9;
+    float4 positionCS               : TEXCOORD10;
+    int mainLightID              : TEXCOORD11;
+#endif
+    
+    UNITY_VERTEX_INPUT_INSTANCE_ID
+    UNITY_VERTEX_OUTPUT_STEREO
+    
+#else
+    
+    LIGHTING_COORDS(7,8)
+    UNITY_FOG_COORDS(9)
+
+#endif
+
+};
+
+// Abstraction over Light shading data.
+struct UtsLight
+{
+    float3 direction;
+    float3 color;
+    float distanceAttenuation;
+    real shadowAttenuation;
+    int type;
+};
+
+void InitializeStandardLitSurfaceDataUTS(float2 uv, out SurfaceData outSurfaceData)
 {
     outSurfaceData = (SurfaceData) 0;
     // half4 albedoAlpha = SampleAlbedoAlpha(uv, TEXTURE2D_ARGS(_BaseMap, sampler_BaseMap));
@@ -112,97 +178,21 @@ half3 GlobalIlluminationUTS(BRDFData brdfData, half3 bakedGI, half occlusion, ha
     return EnvironmentBRDF(brdfData, indirectDiffuse, indirectSpecular, fresnelTerm);
 }
 
-struct VertexInput
+//function to rotate the UV: RotateUV()
+//float2 rotatedUV = RotateUV(i.uv0, (_angular_Verocity*3.141592654), float2(0.5, 0.5), _Time.g);
+float2 RotateUV(float2 _uv, float _radian, float2 _piv, float _time)
 {
-    float4 vertex : POSITION;
-    float3 normal : NORMAL;
-    float4 tangent : TANGENT;
-    float2 texcoord0 : TEXCOORD0;
-
-#ifdef _IS_ANGELRING_OFF
-	float2 lightmapUV   : TEXCOORD1;
-#elif _IS_ANGELRING_ON
-    float2 texcoord1 : TEXCOORD1;
-	float2 lightmapUV   : TEXCOORD2;
-#endif
-	UNITY_VERTEX_INPUT_INSTANCE_ID
-};
-
-struct VertexOutput
-{
-    float4 pos : SV_POSITION;
-    float2 uv0 : TEXCOORD0;
-
-//v.2.0.4
-#ifdef _IS_ANGELRING_OFF
-    float4 posWorld : TEXCOORD1;
-    float3 normalDir : TEXCOORD2;
-    float3 tangentDir : TEXCOORD3;
-    float3 bitangentDir : TEXCOORD4;
-    //v.2.0.7
-    float mirrorFlag : TEXCOORD5;
-
-	DECLARE_LIGHTMAP_OR_SH(lightmapUV, vertexSH, 6);
-#if defined(_ADDITIONAL_LIGHTS_VERTEX) || (VERSION_LOWER(12, 0))
-	half4 fogFactorAndVertexLight   : TEXCOORD7; // x: fogFactor, yzw: vertex light
-#else
-	half  fogFactor					: TEXCOORD7; 
-#endif 
-
-# ifndef _MAIN_LIGHT_SHADOWS
-	float4 positionCS               : TEXCOORD8;
-    int   mainLightID              : TEXCOORD9;
-# else
-	float4 shadowCoord              : TEXCOORD8;
-	float4 positionCS               : TEXCOORD9;
-    int   mainLightID              : TEXCOORD10;
-# endif
-	UNITY_VERTEX_INPUT_INSTANCE_ID
-	UNITY_VERTEX_OUTPUT_STEREO
+    float RotateUV_ang = _radian;
+    float RotateUV_cos = cos(_time * RotateUV_ang);
+    float RotateUV_sin = sin(_time * RotateUV_ang);
+    return (mul(_uv - _piv, float2x2(RotateUV_cos, -RotateUV_sin, RotateUV_sin, RotateUV_cos)) + _piv);
+}
 
 //
-#elif _IS_ANGELRING_ON
-    float2 uv1 : TEXCOORD1;
-    float4 posWorld : TEXCOORD2;
-    float3 normalDir : TEXCOORD3;
-    float3 tangentDir : TEXCOORD4;
-    float3 bitangentDir : TEXCOORD5;
-    //v.2.0.7
-    float mirrorFlag : TEXCOORD6;
-
-    DECLARE_LIGHTMAP_OR_SH(lightmapUV, vertexSH, 7);
-#if defined(_ADDITIONAL_LIGHTS_VERTEX) || (VERSION_LOWER(12, 0))
-    half4 fogFactorAndVertexLight   : TEXCOORD8; // x: fogFactor, yzw: vertex light
-#else
-	half  fogFactor					: TEXCOORD8; // x: fogFactor, yzw: vertex light
-#endif
-    
-# ifndef _MAIN_LIGHT_SHADOWS
-    float4 positionCS               : TEXCOORD9;
-    int   mainLightID              : TEXCOORD10;
-# else
-    float4 shadowCoord              : TEXCOORD9;
-    float4 positionCS               : TEXCOORD10;
-    int   mainLightID              : TEXCOORD11;
-# endif
-    UNITY_VERTEX_INPUT_INSTANCE_ID
-    UNITY_VERTEX_OUTPUT_STEREO
-#else
-    LIGHTING_COORDS(7,8)
-    UNITY_FOG_COORDS(9)
-#endif
-
-};
- 
-// Abstraction over Light shading data.
-struct UtsLight
+fixed3 DecodeLightProbe(fixed3 N)
 {
-    float3 direction;
-    float3 color;
-    float distanceAttenuation;
-    real shadowAttenuation;
-    int type;
-};
+    return ShadeSH9(float4(N, 1));
+}
 
 ///////////////////////////////////////////////////////////////////////////////
 //                      Light Abstraction                                    //
@@ -225,7 +215,7 @@ real MainLightRealtimeShadowUTS(float4 shadowCoord, float4 positionCS)
 
 real AdditionalLightRealtimeShadowUTS(int lightIndex, float3 positionWS, float4 positionCS)
 {
-#if  defined(UTS_USE_RAYTRACING_SHADOW)
+#if defined(UTS_USE_RAYTRACING_SHADOW)
     float w = (positionCS.w == 0) ? 0.00001 : positionCS.w;
     float4 screenPos = ComputeScreenPos(positionCS / w);
     return SAMPLE_TEXTURE2D(_RaytracedHardShadow, sampler_RaytracedHardShadow, screenPos);
@@ -261,10 +251,12 @@ UtsLight GetUrpMainUtsLight()
     light.direction = _MainLightPosition.xyz;
     // unity_LightData.z is 1 when not culled by the culling mask, otherwise 0.
     light.distanceAttenuation = unity_LightData.z;
+
 #if defined(LIGHTMAP_ON) || defined(_MIXED_LIGHTING_SUBTRACTIVE)
     // unity_ProbesOcclusion.x is the mixed light probe occlusion data
     light.distanceAttenuation *= unity_ProbesOcclusion.x;
 #endif
+    
     light.shadowAttenuation = 1.0;
     light.color = _MainLightColor.rgb;
     light.type = _MainLightPosition.w;
@@ -364,13 +356,13 @@ int DetermineUTS_MainLightIndex(float3 posW, float4 shadowCoord, float4 position
         mainLightIndex = MAINLIGHT_IS_MAINLIGHT;
     }
     int lightCount = GetAdditionalLightsCount();
-    for (int ii = 0; ii < lightCount; ++ii)
+    for (int i = 0; i < lightCount; ++i)
     {
-        nextLight = GetAdditionalUtsLight(ii, posW, positionCS);
+        nextLight = GetAdditionalUtsLight(i, posW, positionCS);
         if (nextLight.distanceAttenuation > mainLight.distanceAttenuation && nextLight.type == 0)
         {
             mainLight = nextLight;
-            mainLightIndex = ii;
+            mainLightIndex = i;
         }
     }
 
@@ -390,84 +382,6 @@ UtsLight GetMainUtsLightByID(int index, float3 posW, float4 shadowCoord, float4 
         return GetUrpMainUtsLight(shadowCoord, positionCS);
     }
     return GetAdditionalUtsLight(index, posW, positionCS);
-}
-
-VertexOutput vert(VertexInput v)
-{
-    VertexOutput o = (VertexOutput) 0;
-
-    UNITY_SETUP_INSTANCE_ID(v);
-    UNITY_TRANSFER_INSTANCE_ID(v, o);
-    UNITY_INITIALIZE_VERTEX_OUTPUT_STEREO(o);
-
-    o.uv0 = v.texcoord0;
-
-//v.2.0.4
-#ifdef _IS_ANGELRING_OFF
-//
-#elif _IS_ANGELRING_ON
-    o.uv1 = v.texcoord1;
-#endif
-    o.normalDir = UnityObjectToWorldNormal(v.normal);
-    o.tangentDir = normalize(mul(unity_ObjectToWorld, float4(v.tangent.xyz, 0.0)).xyz);
-    o.bitangentDir = normalize(cross(o.normalDir, o.tangentDir) * v.tangent.w);
-    o.posWorld = mul(unity_ObjectToWorld, v.vertex);
-
-    o.pos = UnityObjectToClipPos(v.vertex);
-    //v.2.0.7 Detection of the inside the mirror (right or left-handed) o.mirrorFlag = -1 then "inside the mirror".
-
-    float3 crossFwd = cross(UNITY_MATRIX_V[0].xyz, UNITY_MATRIX_V[1].xyz);
-    o.mirrorFlag = dot(crossFwd, UNITY_MATRIX_V[2].xyz) < 0 ? 1 : -1;
-    
-    //
-    float3 positionWS = TransformObjectToWorld(v.vertex.xyz);
-    float4 positionCS = TransformWorldToHClip(positionWS);
-    half3 vertexLight = VertexLighting(o.posWorld.xyz, o.normalDir);
-    half fogFactor = ComputeFogFactor(positionCS.z);
-
-    OUTPUT_LIGHTMAP_UV(v.lightmapUV, unity_LightmapST, o.lightmapUV);
-    OUTPUT_SH(o.normalDir.xyz, o.vertexSH);
-
-#if defined(_ADDITIONAL_LIGHTS_VERTEX) ||  (VERSION_LOWER(12, 0))  
-    o.fogFactorAndVertexLight = half4(fogFactor, vertexLight);
-#else
-	o.fogFactor = fogFactor;
-#endif 
-                
-    o.positionCS = positionCS;
-#if defined(_MAIN_LIGHT_SHADOWS) && !defined(_RECEIVE_SHADOWS_OFF)
-#if SHADOWS_SCREEN
-    o.shadowCoord = ComputeScreenPos(positionCS);
-#else
-    o.shadowCoord = TransformWorldToShadowCoord(o.posWorld.xyz);
-#endif
-    o.mainLightID = DetermineUTS_MainLightIndex(o.posWorld.xyz, o.shadowCoord, positionCS);
-#else
-    o.mainLightID = DetermineUTS_MainLightIndex(o.posWorld.xyz, 0, positionCS);
-#endif
-		
-    return o;
-}
-
-
-
-#if defined(_SHADINGGRADEMAP)
-
-#include "UniversalToonBodyShadingGradeMap.hlsl"
-
-#else //#if defined(_SHADINGGRADEMAP)
-
-#include "UniversalToonBodyDoubleShadeWithFeather.hlsl"
-
-#endif //#if defined(_SHADINGGRADEMAP)
-
-float4 frag(VertexOutput i, fixed facing : VFACE) : SV_TARGET
-{
-#if defined(_SHADINGGRADEMAP)
-    return fragShadingGradeMap(i, facing);
-#else
-    return fragDoubleShadeFeather(i, facing);
-#endif
 }
 
 #endif
--- Assets/TestSSU/Outline.hlsl
+++ Assets/TestSSU/Outline.hlsl
@@ -1,12 +1,8 @@
 #ifndef Outline
 #define Outline
 
-#ifndef SHADER_API_D3D11
-#define SHADER_API_D3D11
-#endif
-
-#include "Packages/com.unity.render-pipelines.core/ShaderLibrary/Common.hlsl"
-#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+#include "Input.hlsl"
+#include "Head.hlsl"
 
 uniform float4 _LightColor0;
 
@@ -19,6 +15,7 @@ struct VertexInput
 
     UNITY_VERTEX_INPUT_INSTANCE_ID
 };
+
 struct VertexOutput
 {
     float4 pos : SV_POSITION;
@@ -30,7 +27,6 @@ struct VertexOutput
     UNITY_VERTEX_OUTPUT_STEREO
 };
 
-
 VertexOutput OutlineVertex(VertexInput v)
 {
     VertexOutput o = (VertexOutput) 0;
@@ -39,7 +35,6 @@ VertexOutput OutlineVertex(VertexInput v)
     UNITY_INITIALIZE_VERTEX_OUTPUT_STEREO(o);
 
     o.uv0 = v.texcoord0;
-    float4 objPos = mul(unity_ObjectToWorld, float4(0, 0, 0, 1));
     float2 Set_UV0 = o.uv0;
     float4 _Outline_Sampler_var = tex2Dlod(_Outline_Sampler, float4(TRANSFORM_TEX(Set_UV0, _Outline_Sampler), 0.0, 0));
     
@@ -51,35 +46,35 @@ VertexOutput OutlineVertex(VertexInput v)
     
     //UnpackNormal() can't be used, and so as follows. Do not specify a bump for the texture to be used.
     float4 _BakedNormal_var = (tex2Dlod(_BakedNormal, float4(TRANSFORM_TEX(Set_UV0, _BakedNormal), 0.0, 0)) * 2 - 1);
-    float3 _BakedNormalDir = normalize(mul(_BakedNormal_var.rgb, tangentTransform));
+    float3 _BakedNormalDir = normalize(mul(_BakedNormal_var.xyz, tangentTransform));
     //end
     
-    float Set_Outline_Width = (_Outline_Width * 0.001 * smoothstep(_Farthest_Distance, _Nearest_Distance, distance(objPos.rgb, _WorldSpaceCameraPos)) * _Outline_Sampler_var.rgb).r;
+    float4 objPos = mul(unity_ObjectToWorld, float4(0, 0, 0, 1));
+    float Set_Outline_Width = (_Outline_Width * 0.001 * smoothstep(_Farthest_Distance, _Nearest_Distance, distance(objPos.xyz, _WorldSpaceCameraPos)) * _Outline_Sampler_var.rgb).r;
     Set_Outline_Width *= (1.0f - _ZOverDrawMode);
+    
+    //v.2.0.7
+//#if defined(UNITY_REVERSED_Z)
+//    //v.2.0.4.2 (DX)
+//    _Offset_Z = _Offset_Z * -0.01;
+//#else
+//    //OpenGL
+//    _Offset_Z = _Offset_Z * 0.01;
+//#endif
+    
+    //v2.0.4
+#define _OUTLINE_NML
+#if defined(_OUTLINE_NML)
+    //v.2.0.4.3 baked Normal Texture for Outline
+    o.pos = UnityObjectToClipPos(lerp(float4(v.vertex.xyz + v.normal * Set_Outline_Width, 1), float4(v.vertex.xyz + _BakedNormalDir * Set_Outline_Width, 1), _Is_BakedNormal));
+#elif defined(_OUTLINE_POS)
+    Set_Outline_Width = Set_Outline_Width * 2;
+    float signVar = dot(normalize(v.vertex.xyz), normalize(v.normal)) < 0 ? -1 : 1;
+    o.pos = UnityObjectToClipPos(float4(v.vertex.xyz + signVar * normalize(v.vertex.xyz) * Set_Outline_Width, 1));
+#endif
     
     //v.2.0.7.5
     float4 _ClipCameraPos = mul(UNITY_MATRIX_VP, float4(_WorldSpaceCameraPos.xyz, 1));
-    
-    //v.2.0.7
-#if defined(UNITY_REVERSED_Z)
-    //v.2.0.4.2 (DX)
-    _Offset_Z = _Offset_Z * -0.01;
-#else
-    //OpenGL
-    _Offset_Z = _Offset_Z * 0.01;
-#endif
-    
-//v2.0.4
-#ifdef _OUTLINE_NML
-    //v.2.0.4.3 baked Normal Texture for Outline
-    o.pos = UnityObjectToClipPos(lerp(float4(v.vertex.xyz + v.normal*Set_Outline_Width,1), float4(v.vertex.xyz + _BakedNormalDir*Set_Outline_Width,1),_Is_BakedNormal));
-#elif _OUTLINE_POS
-    Set_Outline_Width = Set_Outline_Width*2;
-    float signVar = dot(normalize(v.vertex),normalize(v.normal))<0 ? -1 : 1;
-    o.pos = UnityObjectToClipPos(float4(v.vertex.xyz + signVar*normalize(v.vertex)*Set_Outline_Width, 1));
-#endif
-    
-    //v.2.0.7.5
     o.pos.z = o.pos.z + _Offset_Z * _ClipCameraPos.z;
     return o;
 }
@@ -91,8 +86,6 @@ float4 OutlineFragment(VertexOutput i) : SV_Target
     {
         return float4(1.0f, 1.0f, 1.0f, 1.0f);
     }
-    _Color = _BaseColor;
-    float4 objPos = mul(unity_ObjectToWorld, float4(0, 0, 0, 1));
     
     //v.2.0.7.5
     half3 ambientSkyColor = unity_AmbientSky.rgb > 0.05 ? unity_AmbientSky.rgb * _Unlit_Intensity : half3(0.05, 0.05, 0.05) * _Unlit_Intensity;
@@ -100,6 +93,7 @@ float4 OutlineFragment(VertexOutput i) : SV_Target
     float lightColorIntensity = (0.299 * lightColor.r + 0.587 * lightColor.g + 0.114 * lightColor.b);
     lightColor = lightColorIntensity < 1 ? lightColor : lightColor / lightColorIntensity;
     lightColor = lerp(half3(1.0, 1.0, 1.0), lightColor, _Is_LightColor_Outline);
+    
     float2 Set_UV0 = i.uv0;
     float4 _MainTex_var = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, TRANSFORM_TEX(Set_UV0, _MainTex));
     float3 Set_BaseColor = _BaseColor.rgb * _MainTex_var.rgb;
@@ -109,17 +103,20 @@ float4 OutlineFragment(VertexOutput i) : SV_Target
     float3 _OutlineTex_var = tex2D(_OutlineTex, TRANSFORM_TEX(Set_UV0, _OutlineTex)).rgb;
     
     //v.2.0.7.5
-#ifdef _IS_OUTLINE_CLIPPING_NO
-    float3 Set_Outline_Color = lerp(_Is_BlendBaseColor_var, _OutlineTex_var.rgb*_Outline_Color.rgb*lightColor, _Is_OutlineTex );
-    return float4(Set_Outline_Color,1.0);
-#elif _IS_OUTLINE_CLIPPING_YES
+#define _IS_OUTLINE_CLIPPING_NO
+#if defined(_IS_OUTLINE_CLIPPING_NO)
+    
+    float3 Set_Outline_Color = lerp(_Is_BlendBaseColor_var, _OutlineTex_var.rgb * _Outline_Color.rgb * lightColor, _Is_OutlineTex);
+    return float4(Set_Outline_Color, 1.0);
+    
+#elif defined(_IS_OUTLINE_CLIPPING_YES)
     float4 _ClippingMask_var = SAMPLE_TEXTURE2D(_ClippingMask, sampler_MainTex, TRANSFORM_TEX(Set_UV0, _ClippingMask));
     float Set_MainTexAlpha = _MainTex_var.a;
-    float _IsBaseMapAlphaAsClippingMask_var = lerp( _ClippingMask_var.r, Set_MainTexAlpha, _IsBaseMapAlphaAsClippingMask );
-    float _Inverse_Clipping_var = lerp( _IsBaseMapAlphaAsClippingMask_var, (1.0 - _IsBaseMapAlphaAsClippingMask_var), _Inverse_Clipping );
-    float Set_Clipping = saturate((_Inverse_Clipping_var+_Clipping_Level));
+    float _IsBaseMapAlphaAsClippingMask_var = lerp(_ClippingMask_var.r, Set_MainTexAlpha, _IsBaseMapAlphaAsClippingMask);
+    float _Inverse_Clipping_var = lerp(_IsBaseMapAlphaAsClippingMask_var, (1.0 - _IsBaseMapAlphaAsClippingMask_var), _Inverse_Clipping);
+    float Set_Clipping = saturate((_Inverse_Clipping_var + _Clipping_Level));
     clip(Set_Clipping - 0.5);
-    float4 Set_Outline_Color = lerp( float4(_Is_BlendBaseColor_var,Set_Clipping), float4((_OutlineTex_var.rgb*_Outline_Color.rgb*lightColor),Set_Clipping), _Is_OutlineTex );
+    float4 Set_Outline_Color = lerp(float4(_Is_BlendBaseColor_var, Set_Clipping), float4((_OutlineTex_var.rgb * _Outline_Color.rgb * lightColor), Set_Clipping), _Is_OutlineTex);
     return Set_Outline_Color;
 #endif
 }
